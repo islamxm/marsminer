603defc389d83b95b1dbc886d2f1fe4dde2c355c
Islam <isxmad@gmail.com>
2023-09-11 19:51:15 +0500

    init

---
+++ #src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFuncs from './modules/functions.js';
+import header from './modules/header.js';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  flsFuncs.isWebp();
+  header()
+})
---
+++ #src/js/modules/header.js
@@ -0,0 +1,14 @@
+const header = () => {
+  const header = document.querySelector('.header')
+  if(header) {
+    document.addEventListener('scroll', () => {
+      if(document.documentElement.scrollTop > 10) {
+        header.classList.add('active')
+      } else {
+        header.classList.remove('active')
+      }
+    })
+  }
+}
+
+export default header;
